a0a9578787b863237d0d25116da0c8fbfe407433
abc <abc@DESKTOP-4MS44ML>
2023-01-09 18:13:42 +0530

    deno project

---
+++ demo1/src/demo1/demo_test1.java
@@ -0,0 +1,19 @@
+package demo1;
+
+public class demo_test1 {
+
+	public static void main(String[] args) {
+
+  int a=500;
+  int b=255;
+  
+  System.out.println(a+b);
+  System.out.println(a-b);
+  System.out.println(a*b);
+  System.out.println(a/b);
+
+
+		  
+	}
+
+}
